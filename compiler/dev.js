c59dde76b908f326695928b5cf09c822b51d26a6
Ha Gia Phat <phatghaa@gmail.com>
2023-05-13 15:11:41 +0700

    feat: faster build (#6)

--- compiler/dev.js
+++ compiler/dev.js
@@ -1,10 +1,9 @@
 // @ts-check
 const { Listr } = require("listr2");
-const watcher = require("@parcel/watcher");
 const path = require("path");
 const ora = require("ora");
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
+const watcher = require("@parcel/watcher");
+const { spawn } = require("node:child_process");
 
 (async () => {
   const execa = (await import("execa")).command;
@@ -15,7 +14,7 @@ const exec = util.promisify(require("child_process").exec);
     {
       title: "Clean output",
       task: () =>
-        execa("lerna run clean:assets").catch(() => {
+        execa("yarn clean:assets").catch(() => {
           throw new Error("Cannot remove output directory");
         }),
     },
@@ -23,61 +22,76 @@ const exec = util.promisify(require("child_process").exec);
       title: "Compile React Components",
       task: () =>
         execa("node ./compiler/frameworks/react.compile").catch((error) => {
-          throw new Error("Error compiling React" + error);
+          throw new Error("Error compiling mitosis components" + error);
         }),
     },
     {
       title: "Bundle React",
       task: async () => {
         try {
-          await exec(`lerna run --scope=@cosmology-mitosis/react build`);
+          await execa(`lerna run --scope=@cosmology-mitosis/react build`);
         } catch (error) {
-          throw new Error("Error bundling React " + error);
+          throw new Error("Error bundling react" + error);
         }
       },
     },
     {
       title: "Launch Watcher",
-      task: async (ctx, task) => {
-        task.title = "Watching for changes";
-        const watchDir = path.resolve(process.cwd(), "src");
+      task: () => {
+        return new Listr(
+          [
+            {
+              title: "Recompile Mitosis",
+              task: async () => {
+                const watchDir = path.resolve(process.cwd(), "src");
 
-        const recompile = async () => {
-          try {
-            await exec("node ./compiler/frameworks/react.compile");
-            await exec(`lerna run --scope=@cosmology-mitosis/react build`);
-          } catch (e) {
-            throw e;
-          }
-        };
+                const compileMitosis = async () => {
+                  try {
+                    await execa("node ./compiler/frameworks/react.compile");
+                  } catch (err) {
+                    throw new Error("Error compiling mitosis components");
+                  }
+                };
 
-        let watch = watcher.subscribe(watchDir, (err, _events) => {
-          const spinner = ora(`Watching src/ for changes...`).start();
-          spinner.text = `src/ changed, compiling...`;
+                let watch = watcher.subscribe(watchDir, (err, _events) => {
+                  const spinner = ora(`Watching src/ for changes...`).start();
+                  spinner.text = `src/ changed, compiling...`;
 
-          recompile()
-            .then(() => {
-              spinner.text = "Compiled successfully.";
-              spinner.succeed();
-            })
-            .catch((e) => {
-              spinner.text = `Error compiling ${e.message}.`;
-              spinner.fail();
-            });
+                  compileMitosis()
+                    .then(() => {
+                      spinner.text = "Compiled successfully.";
+                      spinner.succeed();
+                    })
+                    .catch((e) => {
+                      spinner.text = `Error compiling mitosis ${e.message}.`;
+                      spinner.fail();
+                    });
 
-          if (err) {
-            spinner.text = `Error watching src/ for changes ${err?.message}.`;
-            spinner.fail();
-            return;
-          }
-        });
+                  if (err) {
+                    spinner.text = `Error watching src/ for changes ${err?.message}.`;
+                    spinner.fail();
+                    return;
+                  }
+                });
 
-        return watch.then((subscription) => {
-          unsub = () => subscription.unsubscribe();
-        });
-      },
-      options: {
-        persistentOutput: true,
+                return watch.then((subscription) => {
+                  unsub = () => subscription.unsubscribe();
+                });
+              },
+            },
+            {
+              title: "Parcel watch",
+              task: async () => {
+                return spawn(
+                  "lerna run --stream --scope=@cosmology-mitosis/react watch",
+                  [],
+                  { shell: true }
+                ).stdout;
+              },
+            },
+          ],
+          { concurrent: true }
+        );
       },
     },
   ]);
@@ -87,3 +101,7 @@ const exec = util.promisify(require("child_process").exec);
     console.error(err);
   });
 })();
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
