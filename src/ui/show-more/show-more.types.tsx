ae64c997d827c13c5d04dd3c2c81b712612a4390
yangxiaofan <yangxiaofan@gzkit.com.cn>
2023-05-31 21:52:04 +0800

    chore: Update type of show-more

--- src/ui/show-more/show-more.types.tsx
+++ src/ui/show-more/show-more.types.tsx
@@ -1,19 +1,17 @@
 import { BaseComponentProps } from "../../models/components.model";
-
-const percentage = [
-  0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1
-] as const;
-
-// export interface Activity {
-//   id?: string;
-//   type: typeof percentage[number];
-// }
-// type Percentage = 0|0.1|0.2|0.3|0.4|0.5|0.6|0.7|0.8|0.9|1;
-// ;
-
 export interface ShowMoreProps {
-  initialHeightPercent?: typeof percentage[number],
-  showMoreTitle?: string,
-  showLessTitle?: string,
+  initialHeightPercent?:
+  | 0.1
+  | 0.2
+  | 0.3
+  | 0.4
+  | 0.5
+  | 0.6
+  | 0.7
+  | 0.8
+  | 0.9
+  | 1;
+  showMoreTitle?: string;
+  showLessTitle?: string;
   children: BaseComponentProps["children"];
 }
