f14ad8b4702cb01c41d08988219dfc93f26ab8ae
yyyyaaa <phatghaa@gmail.com>
2023-07-01 01:50:03 +0700

    fix: temporarily remove layout animation

--- src/ui/connect-modal/connect-modal.lite.tsx
+++ src/ui/connect-modal/connect-modal.lite.tsx
@@ -1,19 +1,10 @@
-import { useMetadata, onUpdate, useRef } from "@builder.io/mitosis";
-import autoAnimate from "@formkit/auto-animate";
+import { useMetadata } from "@builder.io/mitosis";
 import type { ConnectModalProps } from "./connect-modal.types";
 import { modalContent, modalChildren } from "./connect-modal.css";
 
 useMetadata({ isAttachedToShadowDom: true, scaffolds: ["modal"] });
 
 export default function ConnectModal(props: ConnectModalProps) {
-  const parentRef = useRef<HTMLDivElement>();
-
-  onUpdate(() => {
-    if (parentRef) {
-      autoAnimate(parentRef);
-    }
-  }, [parentRef]);
-
   return (
     // @ts-expect-error
     <ScaffoldModal
@@ -25,7 +16,7 @@ export default function ConnectModal(props: ConnectModalProps) {
       contentClassName={modalContent}
       childrenClassName={modalChildren}
     >
-      <div ref={parentRef}>{props.children}</div>
+      {props.children}
       {/* @ts-expect-error */}
     </ScaffoldModal>
   );
