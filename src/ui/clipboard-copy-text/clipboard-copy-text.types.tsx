048b5a3d1d4a58d6f30dcc5c38d54dd7fef65531
yyyyaaa <phatghaa@gmail.com>
2023-07-05 17:57:08 +0700

    fix: small UI fixes for beta

--- src/ui/clipboard-copy-text/clipboard-copy-text.types.tsx
+++ src/ui/clipboard-copy-text/clipboard-copy-text.types.tsx
@@ -2,6 +2,8 @@ import { BaseComponentProps } from "../../models/components.model";
 
 export interface ClipboardCopyTextProps extends BaseComponentProps {
   text: string;
+  truncate?: "middle" | "end";
+  midTruncateLimit?: "sm" | "md" | "lg";
   onCopied?: () => void;
   className?: string;
 }
