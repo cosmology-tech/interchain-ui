e17f28457314583047384f9f604007d5c59e96d4
luca <uniqueye608@gmail.com>
2023-09-28 19:24:52 +0800

    fix: remove unused components

--- src/ui/link-card/link-card.css.ts
+++
@@ -1,9 +0,0 @@
-import { style } from "@vanilla-extract/css";
-
-export const container = style({
-  textDecoration: "none",
-  transition: "all 0.1s linear",
-  ":hover": {
-    background: "#EEF2F8",
-  },
-});
--- src/ui/product-highlight/index.ts
+++
@@ -1,1 +0,0 @@
-export { default } from "./product-highlight.lite";
