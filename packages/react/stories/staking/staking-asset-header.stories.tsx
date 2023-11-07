039fe63ff80f9a117c2686a94d96e2912f7c028e
yyyyaaa <phatghaa@gmail.com>
2023-11-07 09:48:10 +0700

    fix(storybook): wrong stub data

--- packages/react/stories/staking/staking-asset-header.stories.tsx
+++ packages/react/stories/staking/staking-asset-header.stories.tsx
@@ -1,6 +1,12 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { asset_list, assets } from "@chain-registry/osmosis";
+import { getAssetByDenom } from "@chain-registry/utils";
+import { Asset as OsmosisAsset } from "@chain-registry/types";
 
 import StakingAssetHeader from "../../src/ui/staking-asset-header";
+
+const osmosisAssets: OsmosisAsset[] = [...assets.assets, ...asset_list.assets];
+const OSMO = getAssetByDenom(osmosisAssets, "uosmo");
 
 const meta: Meta<typeof StakingAssetHeader> = {
   component: StakingAssetHeader,
@@ -15,9 +21,8 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    imgSrc:
-      "https://raw.githubusercontent.com/cosmos/chain-registry/master/agoric/images/bld.png",
-    symbol: "JUNO",
+    imgSrc: OSMO.logo_URIs?.png,
+    symbol: OSMO.symbol,
     totalAmount: 232.2898,
     totalPrice: 1013,
     available: 89.231,
