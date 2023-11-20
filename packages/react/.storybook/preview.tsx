386d1a93d47ae707f02b105948779d2ad77bba40
yyyyaaa <phatghaa@gmail.com>
2023-11-21 03:00:11 +0700

    fix: global css for stories and improve chunking

--- packages/react/.storybook/preview.tsx
+++ packages/react/.storybook/preview.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import type { Preview } from "@storybook/react";
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 import WithThemeDecorator from "./WithThemeDecorator";
 import { I18nProvider } from "../src";
+import "./story-global.css";
 
 const preview: Preview = {
   parameters: {
@@ -20,9 +21,9 @@ const preview: Preview = {
   decorators: [
     (story) => (
       <WithThemeDecorator>
-        <I18nProvider locale="en-US" currency="USD" >
-        {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
-        {story()}
+        <I18nProvider locale="en-US" currency="USD">
+          {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
+          {story()}
         </I18nProvider>
       </WithThemeDecorator>
     ),
