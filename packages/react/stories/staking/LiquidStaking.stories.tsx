771b3ed8cad69ce86b3448b15eaf299317e0f6e2
yyyyaaa <phatghaa@gmail.com>
2024-02-06 23:52:16 +0700

    chore(staking): load custom font into liquid staking story

--- packages/react/stories/staking/LiquidStaking.stories.tsx
+++ packages/react/stories/staking/LiquidStaking.stories.tsx
@@ -9,11 +9,10 @@ import ThemeProvider from "../../src/ui/theme-provider";
 import Box from "../../src/ui/box";
 import LiquidStaking from "../../src/ui/liquid-staking";
 import { LiquidStakingProps } from "../../src/ui/liquid-staking/liquid-staking.types";
-import BasicModal from "../../src/ui/basic-modal";
-import Button from "../../src/ui/button";
 import Text from "../../src/ui/text";
 import Stack from "../../src/ui/stack";
-import Icon from "../../src/ui/icon";
+import useTheme from "../../src/ui/hooks/use-theme";
+import { SYSTEM_FONT_STACK } from "../../src/styles/tokens/typography";
 
 const meta: Meta<typeof LiquidStaking> = {
   component: LiquidStaking,
@@ -40,14 +39,12 @@ export const Primary: Story = {
 
     const { isReady, assets } = useMockData({
       onReady: (assets) => {
-        console.log("onready");
         const stride = assets.find((item) => item.symbol === "STRD");
 
         setStakeToken({
           ...assets[0],
           available: 440.22,
         });
-        console.log("stire", stride, assets);
 
         setReward({
           imgSrc: stride?.imgSrc ?? "",
@@ -159,6 +156,9 @@ const customThemeDefs = [
   {
     name: "dydx",
     vars: {
+      font: {
+        body: ["Montserrat", SYSTEM_FONT_STACK].join(`, `),
+      },
       colors: {
         accent: "#7774FF",
         accentText: "#0E0E0F",
@@ -198,6 +198,7 @@ const customThemeDefs = [
 export const CustomTheme: Story = {
   args: {},
   render: (props) => {
+    const { themeClass, getThemeRef } = useTheme();
     const [stakeToken, setStakeToken] = useState<
       LiquidStakingProps["stakeToken"] | null
     >(null);
@@ -208,14 +209,12 @@ export const CustomTheme: Story = {
 
     const { isReady, assets } = useMockData({
       onReady: (assets) => {
-        console.log("onready");
         const stride = assets.find((item) => item.symbol === "STRD");
 
         setStakeToken({
           ...assets[0],
           available: 440.22,
         });
-        console.log("stire", stride, assets);
 
         setReward({
           imgSrc: stride?.imgSrc ?? "",
@@ -234,94 +233,96 @@ export const CustomTheme: Story = {
 
     return (
       <ThemeProvider themeDefs={customThemeDefs} customTheme="dydx">
-        <LiquidStaking
-          stakeAmount={stakedAmount}
-          stakeToken={stakeToken}
-          reward={reward}
-          precision={6}
-          bottomLink={{
-            href: "https://cosmology.zone/",
-            label: "Learn more",
-          }}
-          timeEstimateLabel="30 seconds"
-          descriptionList={[
-            {
-              title: "Rewards",
-              subtitle: "19.42%",
-              desc: "Rewards on Stride are similar to native staking rewards. However, they accumulate in the background, which reflects in the price of the stToken continually appreciating compared to the native token, while you can still move the stToken around the ecosystem freely.",
-            },
-            {
-              title: "Fees",
-              subtitle: "Low",
-              desc: "Strides 10% fee is only applied to rewards you earn. The tokens you stake (aka principal) and transactions are fee-free!",
-            },
-            {
-              title: "Unbonding",
-              subtitle: "21-24 days",
-              desc: "Unstaking on Stride requires an unbonding period before you can withdraw your tokens. If you don't want to wait, you can sell stATOM directly on an exchange.",
-            },
-            {
-              title: "Value of 1 stATOM",
-              subtitle: "1.204 ATOM",
-              desc: "The value of 1 stATOM if redeemed through the Stride protocol redemption rate grows predictably as staking rewards accrue.",
-            },
-            {
-              title: "Total Value Locked",
-              subtitle: "$24.7m",
-              desc: "The total value of ATOM locked on Stride.",
-            },
-          ]}
-          onChange={(stakeAmount) => {
-            console.log("staked", stakeAmount);
-            setStakedAmount(stakeAmount);
-            setReward((prevReward) => {
-              // This is just mock reward calculation
-              const amt = random(100, 1000, true);
-              const notional = random(500, 20000, true);
+        <div ref={getThemeRef} className={themeClass}>
+          <LiquidStaking
+            stakeAmount={stakedAmount}
+            stakeToken={stakeToken}
+            reward={reward}
+            precision={6}
+            bottomLink={{
+              href: "https://cosmology.zone/",
+              label: "Learn more",
+            }}
+            timeEstimateLabel="30 seconds"
+            descriptionList={[
+              {
+                title: "Rewards",
+                subtitle: "19.42%",
+                desc: "Rewards on Stride are similar to native staking rewards. However, they accumulate in the background, which reflects in the price of the stToken continually appreciating compared to the native token, while you can still move the stToken around the ecosystem freely.",
+              },
+              {
+                title: "Fees",
+                subtitle: "Low",
+                desc: "Strides 10% fee is only applied to rewards you earn. The tokens you stake (aka principal) and transactions are fee-free!",
+              },
+              {
+                title: "Unbonding",
+                subtitle: "21-24 days",
+                desc: "Unstaking on Stride requires an unbonding period before you can withdraw your tokens. If you don't want to wait, you can sell stATOM directly on an exchange.",
+              },
+              {
+                title: "Value of 1 stATOM",
+                subtitle: "1.204 ATOM",
+                desc: "The value of 1 stATOM if redeemed through the Stride protocol redemption rate grows predictably as staking rewards accrue.",
+              },
+              {
+                title: "Total Value Locked",
+                subtitle: "$24.7m",
+                desc: "The total value of ATOM locked on Stride.",
+              },
+            ]}
+            onChange={(stakeAmount) => {
+              console.log("staked", stakeAmount);
+              setStakedAmount(stakeAmount);
+              setReward((prevReward) => {
+                // This is just mock reward calculation
+                const amt = random(100, 1000, true);
+                const notional = random(500, 20000, true);
 
-              return {
-                ...prevReward,
-                symbol: prevReward?.symbol ?? "",
-                imgSrc: prevReward?.imgSrc ?? "",
-                name: prevReward?.name ?? "",
-                rewardAmount: amt,
-                priceDisplayAmount: notional,
-              };
-            });
-          }}
-          footerLabel={
-            <Stack
-              direction="horizontal"
-              space="$4"
-              attributes={{
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Text fontWeight="$normal" fontSize="$sm" color="#A7B4C2">
-                Powered by
-              </Text>
-              <Box
-                as="img"
-                height="$8"
-                width="auto"
+                return {
+                  ...prevReward,
+                  symbol: prevReward?.symbol ?? "",
+                  imgSrc: prevReward?.imgSrc ?? "",
+                  name: prevReward?.name ?? "",
+                  rewardAmount: amt,
+                  priceDisplayAmount: notional,
+                };
+              });
+            }}
+            footerLabel={
+              <Stack
+                direction="horizontal"
+                space="$4"
                 attributes={{
-                  src: strideLogo,
-                  alt: "Stride",
+                  justifyContent: "center",
+                  alignItems: "center",
                 }}
-              />
-            </Stack>
-          }
-          submitButtonProps={{
-            intent: "primary",
-            variant: "solid",
-          }}
-          onSubmit={() => {
-            alert(
-              `Staked [${stakeToken?.symbol}]: amt ${stakedAmount} rward ${reward.rewardAmount}`
-            );
-          }}
-        />
+              >
+                <Text fontWeight="$normal" fontSize="$sm" color="#A7B4C2">
+                  Powered by
+                </Text>
+                <Box
+                  as="img"
+                  height="$8"
+                  width="auto"
+                  attributes={{
+                    src: strideLogo,
+                    alt: "Stride",
+                  }}
+                />
+              </Stack>
+            }
+            submitButtonProps={{
+              intent: "primary",
+              variant: "solid",
+            }}
+            onSubmit={() => {
+              alert(
+                `Staked [${stakeToken?.symbol}]: amt ${stakedAmount} rward ${reward.rewardAmount}`
+              );
+            }}
+          />
+        </div>
       </ThemeProvider>
     );
   },
