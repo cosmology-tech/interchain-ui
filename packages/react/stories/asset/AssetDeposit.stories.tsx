140db934f6ce908516d95464187ce84cf00b966e
yyyyaaa <phatghaa@gmail.com>
2024-03-19 01:00:59 +0700

    refactor(story): deposit and withdraw section

---
+++ packages/react/stories/asset/AssetDeposit.stories.tsx
@@ -0,0 +1,168 @@
+import React, { useState, useEffect } from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import AssetWithdrawTokens from "../../src/ui/asset-withdraw-tokens";
+import BasicModal from "../../src/ui/basic-modal";
+import Button from "../../src/ui/button";
+import Text from "../../src/ui/text";
+import Stack from "../../src/ui/stack";
+import { useMockData } from "../stub/mock-data-client";
+
+const meta: Meta<typeof AssetWithdrawTokens> = {
+  component: AssetWithdrawTokens,
+  title: "Asset/Deposit",
+  tags: ["autodocs"],
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const DepositAsset: Story = {
+  args: {
+    fromSymbol: "UMEE",
+    fromName: "Umee",
+    fromAddress: "umee1lqsq...pv4axdaxk",
+    fromImgSrc:
+      "https://raw.githubusercontent.com/cosmos/chain-registry/master/umee/images/umee.svg",
+    toName: "Osmosis",
+    toAddress: "osmo1lqsq...pv48trj5k",
+    toImgSrc:
+      "https://raw.githubusercontent.com/cosmos/chain-registry/master/osmosis/images/osmo.svg",
+    available: 25.89,
+    amount: "",
+    priceDisplayAmount: 0.5,
+    timeEstimateLabel: "20 seconds",
+    onChange: (value) => {
+      console.log("onChange", value);
+    },
+    onTransfer: () => {
+      console.log("onTransfer");
+    },
+    onCancel: () => {
+      console.log("onCancel");
+    },
+    onAddressChange: (value: string) => {
+      console.log("onAddressChange", value);
+    },
+    onAddressConfirm: () => {
+      console.log("onAddressConfirm");
+    },
+  },
+  render: (props) => {
+    const [isDepositOpen, setIsDepositOpen] = useState(false);
+    const [isWithdrawOpen, setIsWithdrawOpen] = useState(false);
+    const { isReady, assets } = useMockData();
+
+    const [fromData, setFromData] = useState({});
+    const [toData, setToData] = useState({});
+
+    useEffect(() => {
+      if (!isReady) return;
+
+      const fromItem = assets.find((item) => item.symbol === "ATOM");
+      const toItem = assets.find((item) => item.symbol === "STRD");
+      if (fromItem || toItem) return;
+
+      setFromData({
+        fromSymbol: fromItem?.symbol,
+        fromDenom: fromItem?.name,
+        fromAddress: fromItem?.address,
+        fromImgSrc: fromItem?.imgSrc,
+      });
+      setToData({
+        toDenom: toItem?.name,
+        toAddress:
+          toItem?.address || "osmo1qx6kgrla69wmz90tn379p4kaux5prdkuzly2tw",
+        toImgSrc: toItem?.imgSrc,
+      });
+    }, [isReady, assets]);
+
+    return (
+      <Stack space="$0" direction="vertical">
+        <Text fontSize="$2xl" fontWeight="$semibold">
+          Deposit
+        </Text>
+        <AssetWithdrawTokens {...props} />
+      </Stack>
+    );
+  },
+};
+
+export const DepositModalView: Story = {
+  args: {
+    fromSymbol: "UMEE",
+    fromName: "Umee",
+    fromAddress: "umee1lqsq...pv4axdaxk",
+    fromImgSrc:
+      "https://raw.githubusercontent.com/cosmos/chain-registry/master/umee/images/umee.svg",
+    toName: "Osmosis",
+    toAddress: "osmo1lqsq...pv48trj5k",
+    toImgSrc:
+      "https://raw.githubusercontent.com/cosmos/chain-registry/master/osmosis/images/osmo.svg",
+    available: 25.89,
+    amount: "",
+    priceDisplayAmount: 0.5,
+    timeEstimateLabel: "20 seconds",
+    onChange: (value) => {
+      console.log("onChange", value);
+    },
+    onTransfer: () => {
+      console.log("onTransfer");
+    },
+    onCancel: () => {
+      console.log("onCancel");
+    },
+    onAddressChange: (value: string) => {
+      console.log("onAddressChange", value);
+    },
+    onAddressConfirm: () => {
+      console.log("onAddressConfirm");
+    },
+  },
+  render: (props) => {
+    const [isDepositOpen, setIsDepositOpen] = useState(false);
+    const [isWithdrawOpen, setIsWithdrawOpen] = useState(false);
+    const { isReady, assets } = useMockData();
+
+    const [fromData, setFromData] = useState({});
+    const [toData, setToData] = useState({});
+
+    useEffect(() => {
+      if (!isReady) return;
+
+      const fromItem = assets.find((item) => item.symbol === "ATOM");
+      const toItem = assets.find((item) => item.symbol === "STRD");
+      setFromData({
+        fromSymbol: fromItem?.symbol,
+        fromDenom: fromItem?.name,
+        fromAddress: fromItem?.address,
+        fromImgSrc: fromItem?.imgSrc,
+      });
+      setToData({
+        toDenom: toItem?.name,
+        toAddress:
+          toItem?.address || "osmo1qx6kgrla69wmz90tn379p4kaux5prdkuzly2tw",
+        toImgSrc: toItem?.imgSrc,
+      });
+    }, [isReady, assets]);
+
+    return (
+      <Stack space="$10">
+        <BasicModal
+          renderTrigger={(triggerProps) => (
+            <Button {...triggerProps} onClick={() => setIsDepositOpen(true)}>
+              Deposit
+            </Button>
+          )}
+          isOpen={isDepositOpen}
+          title="Deposit"
+          onClose={() => setIsDepositOpen(false)}
+        >
+          <AssetWithdrawTokens {...props} />
+        </BasicModal>
+      </Stack>
+    );
+  },
+};
--- packages/react/stories/asset/AssetWithdraw.stories.tsx
+++ packages/react/stories/asset/AssetWithdraw.stories.tsx
@@ -4,12 +4,13 @@ import type { Meta, StoryObj } from "@storybook/react";
 import AssetWithdrawTokens from "../../src/ui/asset-withdraw-tokens";
 import BasicModal from "../../src/ui/basic-modal";
 import Button from "../../src/ui/button";
+import Text from "../../src/ui/text";
 import Stack from "../../src/ui/stack";
 import { useMockData } from "../stub/mock-data-client";
 
 const meta: Meta<typeof AssetWithdrawTokens> = {
   component: AssetWithdrawTokens,
-  title: "Asset/WithdrawTokens",
+  title: "Asset/Withdraw",
   tags: ["autodocs"],
   argTypes: {},
 };
@@ -18,7 +19,79 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const WithdrawAsset: Story = {
+  args: {
+    fromSymbol: "UMEE",
+    fromName: "Umee",
+    fromAddress: "umee1lqsq...pv4axdaxk",
+    fromImgSrc:
+      "https://raw.githubusercontent.com/cosmos/chain-registry/master/umee/images/umee.svg",
+    toName: "Osmosis",
+    toAddress: "osmo1lqsq...pv48trj5k",
+    toImgSrc:
+      "https://raw.githubusercontent.com/cosmos/chain-registry/master/osmosis/images/osmo.svg",
+    available: 25.89,
+    amount: "",
+    priceDisplayAmount: 0.5,
+    timeEstimateLabel: "20 seconds",
+    onChange: (value) => {
+      console.log("onChange", value);
+    },
+    onTransfer: () => {
+      console.log("onTransfer");
+    },
+    onCancel: () => {
+      console.log("onCancel");
+    },
+    onAddressChange: (value: string) => {
+      console.log("onAddressChange", value);
+    },
+    onAddressConfirm: () => {
+      console.log("onAddressConfirm");
+    },
+  },
+  render: (props) => {
+    const [isDepositOpen, setIsDepositOpen] = useState(false);
+    const [isWithdrawOpen, setIsWithdrawOpen] = useState(false);
+    const { isReady, assets } = useMockData();
+
+    const [fromData, setFromData] = useState({});
+    const [toData, setToData] = useState({});
+
+    useEffect(() => {
+      if (!isReady) return;
+
+      const fromItem = assets.find((item) => item.symbol === "ATOM");
+      const toItem = assets.find((item) => item.symbol === "STRD");
+      if (fromItem || toItem) return;
+
+      setFromData({
+        fromSymbol: fromItem?.symbol,
+        fromDenom: fromItem?.name,
+        fromAddress: fromItem?.address,
+        fromImgSrc: fromItem?.imgSrc,
+      });
+      setToData({
+        toDenom: toItem?.name,
+        toAddress:
+          toItem?.address || "osmo1qx6kgrla69wmz90tn379p4kaux5prdkuzly2tw",
+        toImgSrc: toItem?.imgSrc,
+      });
+    }, [isReady, assets]);
+
+    return (
+      <Stack space="$0" direction="vertical">
+        <Text fontSize="$2xl" fontWeight="$semibold">
+          Withdraw
+        </Text>
+
+        <AssetWithdrawTokens {...props} />
+      </Stack>
+    );
+  },
+};
+
+export const WithdrawModalView: Story = {
   args: {
     fromSymbol: "UMEE",
     fromName: "Umee",
@@ -78,18 +151,6 @@ export const Primary: Story = {
 
     return (
       <Stack space="$10">
-        <BasicModal
-          renderTrigger={(triggerProps) => (
-            <Button {...triggerProps} onClick={() => setIsDepositOpen(true)}>
-              Deposit
-            </Button>
-          )}
-          isOpen={isDepositOpen}
-          title="Deposit"
-          onClose={() => setIsDepositOpen(false)}
-        >
-          <AssetWithdrawTokens {...props} />
-        </BasicModal>
         <BasicModal
           renderTrigger={(triggerProps) => (
             <Button {...triggerProps} onClick={() => setIsWithdrawOpen(true)}>
