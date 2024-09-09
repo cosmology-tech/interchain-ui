72dafaafe5baf49b1bdc3dce06e11f12ea1aadcd
yyyyaaa <phatghaa@gmail.com>
2024-09-09 22:14:21 +0700

    fix

--- packages/vue-codemod/__tests__/vue-codemod.test.ts
+++ packages/vue-codemod/__tests__/vue-codemod.test.ts
@@ -2,7 +2,15 @@ import { it, expect } from "vitest";
 import { transform } from "vue-metamorph";
 import { vueMitosisCodeMod } from "../src/plugins/vue-mitosis.plugin";
 
-it("should remove eventHanlders in a v-bind with spread operator", () => {
+function transformCode(source: string) {
+  return transform(source, "file.vue", [vueMitosisCodeMod]).code;
+}
+
+function trimWhitespace(code: string) {
+  return code.replace(/\s+/g, "");
+}
+
+it("should remove eventHandlers in a v-bind with spread operator", () => {
   const source = `
   <template>
   <Box
@@ -93,11 +101,11 @@ it("should remove eventHanlders in a v-bind with spread operator", () => {
 </template>
   `;
 
-  const result = transform(source, "file.vue", [vueMitosisCodeMod]).code;
-  expect(result.replace(/\s+/g, "")).toBe(expected.replace(/\s+/g, ""));
+  const result = transformCode(source);
+  expect(trimWhitespace(result)).toBe(trimWhitespace(expected));
 });
 
-it("should remove eventHanlders in a v-bind with itself as the value", () => {
+it("should remove eventHandlers in a v-bind only containing itself", () => {
   const source = `
   <template>
   <Box
@@ -184,7 +192,97 @@ it("should remove eventHanlders in a v-bind with itself as the value", () => {
   ></Box>
 </template>
   `;
+  const result = transformCode(source);
+  expect(trimWhitespace(result)).toBe(trimWhitespace(expected));
+});
 
-  const result = transform(source, "file.vue", [vueMitosisCodeMod]).code;
-  expect(result).toBe(expected);
+it("should remove eventHandlers in a v-bind only containing itself in a spread", () => {
+  const source = `
+  <template>
+  <Box
+    :boxRef="buttonRef"
+    :class="combinedClassName"
+    v-bind="{ ...eventHandlers }"
+    ><Spinner
+      :size="iconSize"
+      :attributes="{
+        display:
+          isLoading && spinnerPlacement === 'start' ? 'inline-block' : 'none',
+      }"
+    ></Spinner
+    ><Icon
+      :name="leftIcon"
+      :size="iconSize"
+      :attributes="{
+        display: !!leftIcon && !isLoading ? 'inline-block' : 'none',
+        marginRight: !$slots.default ? '$0' : '$2',
+      }"
+    ></Icon>
+    <template v-if="!isLoading">
+      <slot />
+    </template>
+
+    <Icon
+      :name="rightIcon"
+      :size="iconSize"
+      :attributes="{
+        display: !!rightIcon && !isLoading ? 'inline-block' : 'none',
+        marginLeft: !$slots.default ? '$0' : '$2',
+      }"
+    ></Icon
+    ><Spinner
+      :size="iconSize"
+      :attributes="{
+        display:
+          isLoading && spinnerPlacement === 'end' ? 'inline-block' : 'none',
+      }"
+    ></Spinner
+  ></Box>
+</template>
+  `;
+
+  const expected = `
+  <template>
+  <Box
+    :boxRef="buttonRef"
+    :class="combinedClassName"
+    ><Spinner
+      :size="iconSize"
+      :attributes="{
+        display:
+          isLoading && spinnerPlacement === 'start' ? 'inline-block' : 'none',
+      }"
+    ></Spinner
+    ><Icon
+      :name="leftIcon"
+      :size="iconSize"
+      :attributes="{
+        display: !!leftIcon && !isLoading ? 'inline-block' : 'none',
+        marginRight: !$slots.default ? '$0' : '$2',
+      }"
+    ></Icon>
+    <template v-if="!isLoading">
+      <slot />
+    </template>
+
+    <Icon
+      :name="rightIcon"
+      :size="iconSize"
+      :attributes="{
+        display: !!rightIcon && !isLoading ? 'inline-block' : 'none',
+        marginLeft: !$slots.default ? '$0' : '$2',
+      }"
+    ></Icon
+    ><Spinner
+      :size="iconSize"
+      :attributes="{
+        display:
+          isLoading && spinnerPlacement === 'end' ? 'inline-block' : 'none',
+      }"
+    ></Spinner
+  ></Box>
+</template>
+  `;
+  const result = transformCode(source);
+  expect(trimWhitespace(result)).toBe(trimWhitespace(expected));
 });
--- packages/vue-codemod/src/plugins/vue-mitosis.plugin.ts
+++ packages/vue-codemod/src/plugins/vue-mitosis.plugin.ts
@@ -1,4 +1,5 @@
 import type { CodemodPlugin, AST } from "vue-metamorph";
+import { isEmpty } from "lodash";
 
 const eventMappings = {
   onClick: "click",
@@ -100,53 +101,100 @@ export const vueMitosisCodeMod: CodemodPlugin = {
             node.key.name.name === "bind";
 
           if (isVBindNode) {
-            console.log("isVBindNode", node);
-            const containsObjectExpression =
-              node.value?.type === "VExpressionContainer" &&
-              node.value?.expression?.type === "ObjectExpression";
+            // console.log("isVBindNode", node);
+            const containsExpression =
+              node.value?.type === "VExpressionContainer";
 
             let expression = node.value?.expression;
+
+            if (!containsExpression || !expression) {
+              return;
+            }
+
+            const isObjectExpression = expression.type === "ObjectExpression";
+
+            // Case 1: eventHandlers is in a spread operator
             let originalProperties =
               node?.value?.expression?.type === "ObjectExpression" &&
               node.value.expression.properties
                 ? node.value.expression.properties
                 : null;
 
-            if (
-              !containsObjectExpression ||
-              !expression ||
-              !originalProperties
-            ) {
-              return;
-            }
+            if (isObjectExpression && originalProperties) {
+              const eventHandlersPropertyIndex = originalProperties.findIndex(
+                (property) =>
+                  property.type === "SpreadElement" &&
+                  property.argument.type === "Identifier" &&
+                  property.argument.name === "eventHandlers",
+              );
 
-            // Case 1: eventHandlers is in a spread operator
-            const eventHandlersPropertyNested = originalProperties.find(
-              (property) =>
-                property.type === "SpreadElement" &&
-                property.argument.type === "Identifier" &&
-                property.argument.name === "eventHandlers",
-            );
+              const restProperties = originalProperties.filter(
+                (property, index) => index !== eventHandlersPropertyIndex,
+              );
 
-            const restProperties = originalProperties.filter(
-              (property) =>
-                property.type === "SpreadElement" &&
-                property.argument.type === "Identifier" &&
-                property.argument.name !== "eventHandlers",
-            );
-
-            // Remove eventHandlers from the spread operator
-            if (eventHandlersPropertyNested) {
-              if (node.value?.expression) {
-                // @ts-ignore-next-line
-                node.value.expression.properties = restProperties;
-                transformCount++;
-                return;
+              // Remove eventHandlers from the spread operator
+              if (eventHandlersPropertyIndex !== -1) {
+                // Nothing left to spread, remove the v-bind
+                if (isEmpty(restProperties)) {
+                  if (node.parent.type === "VStartTag") {
+                    // Remove the v-bind
+                    const newAttributes = node.parent.attributes.filter(
+                      (attr) => {
+                        return !(
+                          attr.key.type === "VDirectiveKey" &&
+                          attr.key.name.type === "VIdentifier" &&
+                          attr.key.name.name == "bind" &&
+                          attr.key.name.rawName == "bind"
+                        );
+                      },
+                    );
+                    node.parent.attributes = newAttributes;
+                    transformCount++;
+                    return;
+                  }
+                } else if (node.value?.expression) {
+                  // @ts-ignore-next-line
+                  node.value.expression.properties = restProperties;
+                  transformCount++;
+                  return;
+                }
               }
             }
 
             // Case 2: eventHandlers is not in a spread operator but just a plain object
             // .ie v-bind="eventHandlers"
+            const isVExpressionContainer =
+              node.value?.type === "VExpressionContainer" &&
+              node.value?.expression &&
+              node.value?.expression.type === "Identifier" &&
+              node.value.expression.name === "eventHandlers";
+
+            if (isVExpressionContainer) {
+              const originalAttributes = node.parent.attributes;
+              const eventHandlersAttributeIndex = originalAttributes.findIndex(
+                (attr) => {
+                  return (
+                    attr.type === "VAttribute" &&
+                    attr.directive &&
+                    attr.value?.type === "VExpressionContainer" &&
+                    attr.value?.expression?.type === "Identifier" &&
+                    attr.value?.expression?.name === "eventHandlers"
+                  );
+                },
+              );
+
+              if (eventHandlersAttributeIndex === -1) {
+                return;
+              }
+
+              const restAttributes = originalAttributes.filter(
+                (attr, index) => index !== eventHandlersAttributeIndex,
+              );
+
+              node.parent.attributes = restAttributes;
+              transformCount++;
+              return;
+            }
           }
         },
       });
