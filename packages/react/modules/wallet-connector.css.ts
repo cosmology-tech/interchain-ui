68b182b4498edcf291e0b0d2ba5aa80c1401daf2
Ha Gia Phat <phatghaa@gmail.com>
2024-10-26 22:09:43 +0700

    feat: code split css bundle for wallet connector (#209)

---
+++ packages/react/modules/wallet-connector.css.ts
@@ -0,0 +1,21 @@
+// Custom entry point for the wallet connector styles
+// Contains only the styles that are needed for the wallet connector
+
+// ==== Core components ====
+import "../src/ui/button/button.css";
+import "../src/ui/text/text.css";
+import "../src/ui/avatar/avatar.css";
+import "../src/ui/basic-modal/basic-modal.css";
+import "../src/ui/shared/shared.css";
+import { rainbowSprinkles as _rainbowSprinkles } from "../src/styles/rainbow-sprinkles.css";
+
+// ==== Connect modal components ====
+import "../src/ui/connect-modal/connect-modal.css";
+import "../src/ui/connect-modal-head/connect-modal-head.css";
+import "../src/ui/connect-modal-install-button/connect-modal-install-button.css";
+import "../src/ui/connect-modal-qrcode/connect-modal-qrcode.css";
+import "../src/ui/connect-modal-qrcode-error/connect-modal-qrcode-error.css";
+import "../src/ui/connect-modal-qrcode-skeleton/connect-modal-qrcode-skeleton.css";
+import "../src/ui/connect-modal-status/connect-modal-status.css";
+import "../src/ui/connect-modal-wallet-button/connect-modal-wallet-button.css";
+import "../src/ui/connect-modal-wallet-list/connect-modal-wallet-list.css";
