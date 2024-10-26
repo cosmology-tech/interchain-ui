0bcd3e570ad8f951cdace85e960408f7bc2a0479
yyyyaaa <phatghaa@gmail.com>
2024-10-26 12:18:41 +0700

    chore: remove unused code

--- apps/docs-react/components/logo.tsx
+++
@@ -1,5 +0,0 @@
-import Image from "next/legacy/image";
-
-export const Logo = () => (
-  <Image src="/cosmology.svg" width={40} height={40} alt="Logo" />
-);
--- apps/docs-react/next.config.js
+++
@@ -1,9 +0,0 @@
-const withNextra = require("nextra")({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.jsx",
-});
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = withNextra(nextConfig);
